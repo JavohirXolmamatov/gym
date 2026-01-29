690185f38247e86fa701119a8c36a91e8202ee2d
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2026-01-29 12:29:45 +0500

    fillLoading

--- src/components/shared/fill-loading.tsx
+++ src/components/shared/fill-loading.tsx
@@ -1,5 +1,13 @@
+import { Loader } from "lucide-react";
+
 const FillLoading = () => {
-  return <div>FillLoading</div>;
+  return (
+    <div className="w-full h-full inset-0 absolute bg-black/80  ">
+      <div className="w-full h-full inset-0 flex items-center justify-center">
+        <Loader className="animate-spin" />
+      </div>
+    </div>
+  );
 };
 
 export default FillLoading;
